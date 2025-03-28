999cf24e23321b49575fbc8c9b14c29db0e83efa
yamatatsu <the.kinnikuman@gmail.com>
2025-03-28 16:12:44 +0900

    Initial commit

---
+++ test/push-iot-metrics-to-grafana-mimir.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PushIotMetricsToGrafanaMimir from '../lib/push-iot-metrics-to-grafana-mimir-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/push-iot-metrics-to-grafana-mimir-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PushIotMetricsToGrafanaMimir.PushIotMetricsToGrafanaMimirStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
